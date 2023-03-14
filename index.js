3db665b5f1eae8d6621381055b3c23f50c560320
+ <+>
2023-03-14 13:53:57 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // multiply will be 62
+
+// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+// Write your code here
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4; // mod will be 4
+
+// Write your code here
+let max = Math.max(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 20);
